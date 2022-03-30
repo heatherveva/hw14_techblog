2fc8326a59087d432c85148a7751857e5c09e6e9
Heather Stevens <heatherveva@gmail.com>
2022-03-30 16:59:59 -0500

    build models and renamed files

--- models/Blog.js
+++ models/Blog.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Blog extends Model {}
 
 Project.init(
   {
@@ -11,21 +11,18 @@ Project.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    content: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
     },
     user_id: {
       type: DataTypes.INTEGER,
@@ -44,4 +41,4 @@ Project.init(
   }
 );
 
-module.exports = Project;
+module.exports = Blog;
